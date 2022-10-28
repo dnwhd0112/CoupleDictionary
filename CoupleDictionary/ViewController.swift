88962c19eb5bf46458fd1fb0e63f249846c7d4d5
dnwhd0112 <dnwhd0112@naver.com>
2022-10-28 22:41:54 +0900

    Initial Commit

---
+++ CoupleDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoupleDictionary
+//
+//  Created by 곽우종 on 2022/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
